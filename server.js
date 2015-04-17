0ca0b346aabac93b190f5e1df1422e2bef55f0cb
thbourlove <thbourlove@gmail.com>
2015-04-17 13:35:11 +0800

    clear local storage after initialling page

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var server = prerender({
 // server.use(prerender.basicAuth());
 // server.use(prerender.whitelist());
 server.use(prerender.blacklist());
-// server.use(prerender.logger());
+server.use(prerender.logger());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 // server.use(prerender.inMemoryHtmlCache());
